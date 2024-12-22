58032cd01c07f2f8ea6c5ff430e05d4facf8dce7
SimilachiAndrei <sivfhd@yahoo.com>
2024-12-22 22:29:37 +0200

    added constructor and __new__ to Ethernet class

--- HttpSniffer/main.py
+++ HttpSniffer/main.py
@@ -2,13 +2,20 @@ import socket
 import struct
 from ctypes import *
 
-
 class Ethernet(Structure):
     _fields_ = [
         ("dst", c_ubyte * 6),
         ("src", c_ubyte * 6),
         ("type", c_ushort)
     ]
+
+    def __new__(cls, socket_buffer=None):
+        return cls.from_buffer_copy(socket_buffer)
+
+    def __init__(self, socket_buffer=None):
+        self.dst_mac = ":".join(["{:02x}".format(x) for x in self.dst])
+        self.src_mac = ":".join(["{:02x}".format(x) for x in self.src])
+        self.proto = socket.htons(self.type)
 
 
 class IP(Structure):
