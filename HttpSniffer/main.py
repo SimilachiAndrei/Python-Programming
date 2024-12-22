f302faa52c6468f56220da70fc673bc7a991f3b9
SimilachiAndrei <sivfhd@yahoo.com>
2024-12-22 17:07:03 +0200

    added Ethernet header representation

--- HttpSniffer/main.py
+++ HttpSniffer/main.py
@@ -2,6 +2,13 @@ import socket
 import struct
 from ctypes import *
 
+class Ethernet(Structure):
+    _fields_ = [
+        ("dst", c_ubyte * 6),
+        ("src", c_ubyte * 6),
+        ("type", c_ushort)
+    ]
+
 class IP(Structure):
     _fields_ = [
         ("ihl", c_ubyte, 4),
@@ -27,3 +34,4 @@ class IP(Structure):
         if socket_buffer:
             self.src_address = socket.inet_ntoa(struct.pack("<L", self.src))
             self.dst_address = socket.inet_ntoa(struct.pack("<L", self.dst))
+
