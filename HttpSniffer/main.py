3ca794fb068fce2566b6d494b8299f809a44b137
SimilachiAndrei <sivfhd@yahoo.com>
2024-12-22 16:32:51 +0200

    added IP class with header structure

---
+++ HttpSniffer/main.py
@@ -0,0 +1,21 @@
+import socket
+import struct
+from ctypes import *
+
+
+class IP(Structure):
+    _fields_ = [
+        ("ihl", c_ubyte, 4),
+        ("version", c_ubyte, 4),
+        ("tos", c_ubyte),
+        ("len", c_ushort),
+        ("id", c_ushort),
+        ("offset", c_ushort),
+        ("ttl", c_ubyte),
+        ("protocol_num", c_ubyte),
+        ("sum", c_ushort),
+        ("src", c_uint32),
+        ("dst", c_uint32)
+    ]
+
+
