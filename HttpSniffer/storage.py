b87abdc103e82b43b442baaf14af032ddc9709c8
SimilachiAndrei <sivfhd@yahoo.com>
2024-12-28 21:28:35 +0200

    added function for adding to deque

--- HttpSniffer/storage.py
+++ HttpSniffer/storage.py
@@ -6,3 +6,8 @@ class RequestStorage:
     def __init__(self, max_size=100):
         self.requests = deque(maxlen=max_size)
         self.request_lock = threading.Lock()
+
+    def add_request(self, request_data):
+        with self.request_lock:
+            self.requests.append(request_data)
+            return len(self.requests) - 1
