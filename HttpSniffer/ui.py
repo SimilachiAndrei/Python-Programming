bfb2965cf4b357c56ac16028c238745079776df2
SimilachiAndrei <sivfhd@yahoo.com>
2024-12-28 22:35:19 +0200

    added function for displaying options for inspecting

--- HttpSniffer/ui.py
+++ HttpSniffer/ui.py
@@ -2,19 +2,18 @@ from io import BytesIO
 import gzip
 import sys
 
-
 class UI:
     def __init__(self, request_store):
         self.request_store = request_store
 
-    def _display_menu(self):
+    def display_menu(self):
         print("\nCommands:")
         print("1. List all captured requests")
         print("2. View request details")
         print("3. Exit program")
 
 
-    def _list_requests(self):
+    def list_requests(self):
         requests = self.request_store.list_requests()
         print("\nCaptured Requests:")
         for idx, req in requests:
@@ -22,3 +21,13 @@ class UI:
                 print(f"{idx}. Response: {req['http'].status_code} from {req['ip'].src_address}")
             else:
                 print(f"{idx}. {req['http'].method} to {req['ip'].dst_address}")
+
+
+    def display_detail_options(self):
+        print("\nChoose what information to view (comma-separated):")
+        print("1. Ethernet")
+        print("2. IP")
+        print("3. TCP")
+        print("4. HTTP Headers")
+        print("5. HTTP Payload")
+        print("6. All")
