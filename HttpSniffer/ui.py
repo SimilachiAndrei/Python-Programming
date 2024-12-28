03027c191f3273c4bcb13faeb45beec9171577ed
SimilachiAndrei <sivfhd@yahoo.com>
2024-12-28 22:28:19 +0200

    added menu displaying function

--- HttpSniffer/ui.py
+++ HttpSniffer/ui.py
@@ -8,3 +8,9 @@ class UI:
     def __init__(self, request_store):
         self.request_store = request_store
 
+    def _display_menu(self):
+        print("\nCommands:")
+        print("1. List all captured requests")
+        print("2. View request details")
+        print("3. Exit program")
+
