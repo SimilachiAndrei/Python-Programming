d275387e65a884c4f1263c6cab7e932b6de1aa7c
SimilachiAndrei <sivfhd@yahoo.com>
2024-12-28 22:32:11 +0200

    added request listing function

--- HttpSniffer/ui.py
+++ HttpSniffer/ui.py
@@ -1,4 +1,3 @@
-# ui.py
 from io import BytesIO
 import gzip
 import sys
@@ -14,3 +13,12 @@ class UI:
         print("2. View request details")
         print("3. Exit program")
 
+
+    def _list_requests(self):
+        requests = self.request_store.list_requests()
+        print("\nCaptured Requests:")
+        for idx, req in requests:
+            if req['http'].is_response:
+                print(f"{idx}. Response: {req['http'].status_code} from {req['ip'].src_address}")
+            else:
+                print(f"{idx}. {req['http'].method} to {req['ip'].dst_address}")
